23cdf89db8220e69bc3abe359d58d9de1717737a
muddasir <muddasirabro724@gmail.com>
2025-04-18 10:08:32 +0500

    add limit number

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from random import randint 
 st.title("Number Guessing Game🤔.")
 st.write("Guess the number between 1 to 10.")
-user_num=st.number_input("Guess?",value=0)
+user_num=st.number_input("Guess?",value=0,max_value=10)
 com_num=randint(1,10)
 def guess(user_num,com_num):
     if user_num == com_num:
